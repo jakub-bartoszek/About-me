6dbe6854d776c846ac8029f0f57da52340764952
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-08-17 20:39:45 +0200

    moved footer to another component

---
+++ src/features/aboutMe/Footer/index.jsx
@@ -0,0 +1,11 @@
+import { Email, Wrapper } from "./styled";
+
+export const Footer = () => (
+	<Wrapper>
+		Let's talk
+		<Email>j.bartoszek507@gmail.com</Email>
+		I’m always open to new projects whenever I have the time. If you
+		have a website, dashboard or mobile app in mind and need some help
+		to make your ideas come to life, feel free to conatct me.
+	</Wrapper>
+);
---
+++ src/features/aboutMe/Footer/styled.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.footer`
+	width: 60%;
+`;
+
+export const Email = styled.h3`
+	font-size: 32px;
+	font-weight: 900;
+	line-height: normal;
+	letter-spacing: 1.6px;
+	margin: 24px 0;
+`;
