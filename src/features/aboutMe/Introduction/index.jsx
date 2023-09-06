4e36d8c40d81bc2d10100fe86daded5131f8814d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-09-06 16:16:30 +0200

    added message icon and hire me button

--- src/features/aboutMe/Introduction/index.jsx
+++ src/features/aboutMe/Introduction/index.jsx
@@ -1,17 +1,16 @@
-import { About, Description, Header, Image, Wrapper } from "./styled";
-import { ThemeSwitch } from "../../../common/ThemeSwitch/index";
+import { About, Description, Header, HireMeButton, Image, Message, Wrapper } from "./styled";
 
 export const Introduction = () => (
 	<Wrapper>
 		<Image src="https://i.postimg.cc/WzCSYcCZ/my-Picture.png" />
 		<About>
-			<ThemeSwitch />
 			This is
 			<Header>Jakub Bartoszek</Header>
 			<Description>
-				👨🏻‍💻 I’m a passionate Frontend Developer in love with
-				React, currently looking for new job opportunities.
+				👨🏻‍💻 I’m a passionate Frontend Developer in love with React,
+				currently looking for new job opportunities.
 			</Description>
+			<HireMeButton><Message/>Hire me!</HireMeButton>
 		</About>
 	</Wrapper>
 );
--- src/features/aboutMe/Introduction/styled.jsx
+++ src/features/aboutMe/Introduction/styled.jsx
@@ -1,14 +1,24 @@
 import { styled } from "styled-components";
+import { ReactComponent as MessageIcon } from "../../../common/icons/message.svg";
 
 export const Wrapper = styled.header`
-	display: grid;
-	grid-template-columns: auto 1fr;
+	display: flex;
+	align-items: center;
+	@media (max-width: ${({ theme }) =>
+			theme.breakpoints.mobileMax}px) {
+		display: block;
+		padding: 0 20px;
+	}
 `;
 export const Image = styled.img`
 	border-radius: 50%;
-	width: 398px;
-	height: 398px;
+	max-width: 389px;
+	height: auto;
 	padding: 8px;
+	@media (max-width: ${({ theme }) =>
+			theme.breakpoints.mobileMax}px) {
+		padding: 0;
+	}
 `;
 export const Header = styled.h1`
 	color: ${({ theme }) => theme.colors.textPrimary};
@@ -18,9 +28,30 @@ export const Header = styled.h1`
 `;
 export const About = styled.div`
 	padding-left: 66px;
+	@media (max-width: ${({ theme }) =>
+			theme.breakpoints.mobileMax}px) {
+		padding: 0;
+	}
 `;
 export const Description = styled.p`
 	font-size: 20px;
 	line-height: 140%;
 	letter-spacing: 1px;
+	padding-bottom: 32px;
+`;
+
+export const Message = styled(MessageIcon)``;
+
+export const HireMeButton = styled.button`
+	font-weight: 700;
+	display: inline-flex;
+	padding: 12px 16px;
+	justify-content: center;
+	align-items: center;
+	gap: 16px;
+	color: white;
+	background-color: blue;
+	border-radius: 4px;
+	border: none;
+	background: #0366d6;
 `;
