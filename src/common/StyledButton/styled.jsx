93500a0297cd8bb69d2e9e8bccbb237c7b47feaf
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-09-15 13:53:50 +0200

    made StyledButton common component

---
+++ src/common/StyledButton/styled.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+	font-weight: 700;
+	display: inline-flex;
+	padding: 12px 16px;
+	justify-content: center;
+	align-items: center;
+	gap: 16px;
+	color: white;
+	background-color: blue;
+	border-radius: 4px;
+	border: none;
+	background: #0366d6;
+	transition: 0.3s;
+	cursor: pointer;
+	&:focus-visible {
+		outline: none;
+		scale: 1.1;
+		filter: brightness(110%);
+	}
+	&:hover {
+		scale: 1.1;
+		filter: brightness(110%);
+	}
+	&:active {
+		filter: brightness(130%);
+	}
+`;
