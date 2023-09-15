30063812abbb3fa9f7b41444660028c77212333d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-09-15 13:55:17 +0200

    fixed theme transition on skills

--- src/features/aboutMe/Skills/styled.jsx
+++ src/features/aboutMe/Skills/styled.jsx
@@ -4,6 +4,7 @@ export const Wrapper = styled.section`
 	margin: 72px 0;
 	background-color: ${({ theme }) => theme.colors.tileBackground};
 	padding: 32px;
+	transition: 0.3s;
 `;
 
 export const List = styled.ul`
@@ -13,12 +14,10 @@ export const List = styled.ul`
 	line-height: 140%;
 	letter-spacing: 0.9px;
 	font-size: 18px;
-	@media (max-width: ${({ theme }) =>
-			theme.breakpoints.tabletHorizontalMax}px) {
+	@media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontalMax}px) {
 		grid-template-columns: repeat(2, 1fr);
 	}
-	@media (max-width: ${({ theme }) =>
-			theme.breakpoints.mobileMax}px) {
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
 		grid-template-columns: repeat(1, 1fr);
 	}
 `;
