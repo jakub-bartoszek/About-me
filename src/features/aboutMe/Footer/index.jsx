ba006d7d943fcdf86a402b21e538454fc44161d7
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-13 01:33:08 +0100

    changed email

--- src/features/aboutMe/Footer/index.jsx
+++ src/features/aboutMe/Footer/index.jsx
@@ -4,7 +4,7 @@ import { Email, Wrapper } from "./styled";
 export const Footer = () => (
 	<Wrapper>
 		Let's talk
-		<Email>j.bartoszek507@gmail.com</Email>
+		<Email>jakub.bartoszek.dev@gmail.com</Email>
 		I’m always open to new projects whenever I have the time. If you
 		have a website, dashboard or mobile app in mind and need some help
 		to make your ideas come to life, feel free to conatct me.
